eb13ae6246ee5c0680e09c3a44b8d19859c69479
Gabriel Miranda Carvalho <gabrielm.car@gmail.com>
2025-04-28 22:45:37 -0300

    ajustando projeto

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/gblcarvalho/go-expert-lab-cloud-run/configs"
 	"github.com/gblcarvalho/go-expert-lab-cloud-run/internal/infra/gateways"
 	"github.com/gblcarvalho/go-expert-lab-cloud-run/internal/infra/web"
 	"github.com/go-chi/chi/v5"
@@ -10,14 +11,20 @@ import (
 )
 
 func main() {
+
+	configs, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
 	cepGateway := gateways.NewViaCEPGateway()
-	weatherGateway := gateways.NewWeatherAPIGateway("")
+	weatherGateway := gateways.NewWeatherAPIGateway(configs.WeatherApiKey)
 
 	handler := web.NewWeatherHandler(cepGateway, weatherGateway)
 
 	r.Get("/weather/{cep}", handler.Get)
-	http.ListenAndServe(":8080", r)
+	http.ListenAndServe(":8000", r)
 }
